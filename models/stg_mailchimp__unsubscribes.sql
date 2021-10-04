ccc8e70facde203ab9d79c3c92a2dc7ab6cbf724
fivetran-reneeli <renee.li@fivetran.com>
2021-10-04 23:48:17 +0100

    stg files

---
+++ models/stg_mailchimp__unsubscribes.sql
@@ -0,0 +1,47 @@
+{{ config(enabled=var('using_automations', True)) }}
+
+with base as (
+
+    select * 
+    from {{ ref('stg_mailchimp__unsubscribes_tmp') }}
+
+),
+
+
+fields as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_mailchimp__unsubscribes_tmp')),
+                staging_columns=get_unsubscribe_columns()
+            )
+        }}
+        
+    from base
+
+), 
+
+final as (
+
+    select 
+        campaign_id,
+        member_id,
+        list_id,
+        reason as unsubscribe_reason,
+        timestamp as unsubscribe_timestamp
+    from fields
+
+), 
+
+unique_key as (
+
+    select 
+        *, 
+        {{ dbt_utils.surrogate_key([ 'member_id', 'list_id', 'unsubscribe_timestamp']) }} as id
+    from final
+
+)
+
+select *
+from unique_key
